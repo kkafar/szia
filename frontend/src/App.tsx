ca7d7d5857c3b2eee2b73a4e931b4f2d10a50a15
Kacper Kafara <kacperkafara@gmail.com>
2023-05-24 10:46:07 +0200

    Start dev frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { RoomInfo, RoomState, RoomSettings } from './types';
+import axios from 'axios';
+
+type RoomListProps = {
+  rooms: Array<RoomInfo>,
+};
+
+function RoomList(props: RoomListProps) {
+  return (
+    <div>
+      {props.rooms.map((room) => {
+        return <p>{room.settings.desiredTemperature}</p>
+      })}
+    </div>
+  );
+}
+
+const SERVER_URL = "http://localhost:8080";
+
+
 function App() {
+  const roomIds: Array<string> = ["alfa", "beta", "gamma"];
+  const [roomList, setRoomList] = useState<Array<RoomInfo>>([]);
+
+  function updateState(data: RoomInfo) {
+    console.log("Updating state");
+    setRoomList([data]);
+  }
+
+  const getData = async () => {
+    console.log("Fetching data");
+    try {
+      const result = await axios({
+        method: 'get',
+        baseURL: SERVER_URL,
+        url: '/room/alfa',
+        headers: {
+          'Access-Control-Allow-Origin': 'localhost:8080',
+        }
+      });
+
+      updateState(result.data as RoomInfo);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    const intervalHandle = setInterval(() => {
+      getData();
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalHandle);
+    };
+  }, []);
+
+  const MockRoom: RoomInfo = {
+    settings: {
+      defaultTemperature: 20.0,
+      desiredTemperature: 25.0,
+    },
+    state: {
+      powerAvailable: 300.0,
+      powerConsumed: -30.0,
+      temperature: 19.0,
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
+      <RoomList rooms={roomList} />
     </div>
   );
 }

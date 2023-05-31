b2f07405bcc336906c00e347c797a01354095e6d
Kacper Kafara <kacperkafara@gmail.com>
2023-05-31 09:35:39 +0200

    Refresh plot data periodically (#5)

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -6,22 +6,22 @@ import { useEffect } from "react";
 
 type ListItemProps = {
   title: string,
-  
+
 };
 
 function ListItem(props: ListItemProps) {
-    
+
 };
 
 export default function Room(props: RoomInfo) {
 
   useEffect(() => {
-     
+
   }, [props.state.temperature]);
 
   function resolveBgColor() {
     if (Math.abs(props.state.temperature - props.settings.desiredTemperature) / props.settings.desiredTemperature <= 0.1) {
-      return '#a8c256';  
+      return '#a8c256';
     } else {
       return '#ff6e52';
     }
@@ -30,20 +30,20 @@ export default function Room(props: RoomInfo) {
   return (
     <div className="Room">
       <div className="Room-content" style={{ backgroundColor: resolveBgColor() }}>
-      <div className="Room-list">
-        <p>ID: {props.name ?? "Unknown"}</p>
-        <ul>
-          <li>Default temperature: {props.settings.defaultTemperature.toFixed(2)}</li>
-          <li>Target temperature: {props.settings.desiredTemperature.toFixed(2)}</li>
-          <li>Power available: {props.state.powerAvailable.toFixed(2)}</li>
-          <li>Power consumed: {props.state.powerConsumed.toFixed(2)}</li>
-          <li>Current temperature: {props.state.temperature.toFixed(2)}</li>
-        </ul>
-      </div>
+        <div className="Room-list">
+          <p>ID: {props.name ?? "Unknown"}</p>
+          <ul>
+            <li>Default temperature: {props.settings.defaultTemperature.toFixed(2)}</li>
+            <li>Target temperature: {props.settings.desiredTemperature.toFixed(2)}</li>
+            <li>Power available: {props.state.powerAvailable.toFixed(2)}</li>
+            <li>Power consumed: {props.state.powerConsumed.toFixed(2)}</li>
+            <li>Current temperature: {props.state.temperature.toFixed(2)}</li>
+          </ul>
+        </div>
       </div>
       <div className="Room-filler">
 
       </div>
-    </div>    
+    </div>
   );
 }

9b1b302993acdf245a0a0dac0f4f548bb09d29ee
Kacper Kafara <kacperkafara@gmail.com>
2023-06-14 13:29:48 +0200

    Add commons module with column definitions

---
+++ experiments/src/commons.py
@@ -0,0 +1,29 @@
+COL_TIME = 'time'
+COL_ID = 'roomid'
+COL_INIT_ENERGY = 'init_energy'
+COL_DEF_TEMP = 'default_temp'
+COL_TARGET_TEMP = 'target_temp'
+COL_POW_AV = 'pow_available'
+COL_POW_CONS = 'pow_consumed'
+COL_TEMP = 'temp'
+COL_THERM_CAP = 'thermcap'
+COL_THERM_RES = 'thermres'
+COL_METRIC = 'metric'
+
+GENERAL_DATA_COLUMNS = [
+    COL_TIME,
+    COL_THERM_CAP,
+    COL_THERM_RES,
+    COL_METRIC,
+]
+
+AGENT_DATA_COLUMNS = [
+    COL_TIME,
+    COL_ID,
+    COL_INIT_ENERGY,
+    COL_DEF_TEMP,
+    COL_TARGET_TEMP,
+    COL_POW_AV,
+    COL_POW_CONS,
+    COL_TEMP,
+]
